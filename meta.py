c252eee76702031dd170c4da488ba38a1c44e9e9
Richard Polzin <polzin.richard@gmail.com>
2016-12-18 16:27:36 +0100

    T to t

--- meta.py
+++ meta.py
@@ -12,7 +12,7 @@ polygon is not aligned with the radial axes.
 """
 import numpy as np
 
-import Tkinter
+import tkinter
 import matplotlib
 import matplotlib.backends.backend_tkagg
 import matplotlib.pyplot as plt
